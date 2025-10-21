7b6be1bb2cdd7388489b445fab295a07f2a751db
root <root@DESKTOP-0V4NUDQ.localdomain>
2025-10-21 12:26:26 -0300

    feat: implementação destroy-cicd

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,11 @@
 variable "docker_image_name" {
   description = "Docker image name (e.g. nilsonmazurchi/gerenciador-tarefas)"
   type = string
+  default     = "nilsonmazurchi/gerenciador-tarefas"
 }
 
 variable "docker_image_tag" {
   description = "Docker image tag to deploy"
   type = string
+  default     = "latest"
 }
